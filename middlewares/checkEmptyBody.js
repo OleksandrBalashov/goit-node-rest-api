f4d9e8e5af7c61892ad556b2ebed4ec0ac08b837
OleksandrBalashov <aleksandr.balashov.sax@gmai.com>
2024-05-11 06:34:10 +0200

    change error message

--- middlewares/checkEmptyBody.js
+++ middlewares/checkEmptyBody.js
@@ -4,7 +4,7 @@ const checkEmptyBody = (req, res, next) => {
   const { length } = Object.keys(req.body);
 
   if (!length) {
-    return next(HttpError(400, "Body must have at least 1 key"));
+    return next(HttpError(400, "Body must have at least 1 field"));
   }
 
   next();
